06ece6903606974461021df6291fa3d128567581
Rahmanmohsin <mohsinrahman901@gmail.com>
2025-10-21 21:18:26 +0200

    Add GitHub Actions workflow for auto-build and deployment

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   build: {
     outDir: 'dist',
     sourcemap: false,
     minify: 'terser',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      }
+    }
   },
   server: {
     port: 3000,
